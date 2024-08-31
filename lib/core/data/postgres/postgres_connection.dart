7f7970a9a218d64c79282cc6dec8b4a59956e21f
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2024-08-31 14:08:03 -0500

    Changed to my own docker postgres db, disabled ssl

--- lib/core/data/postgres/postgres_connection.dart
+++ lib/core/data/postgres/postgres_connection.dart
@@ -1,23 +1,23 @@
 import 'package:postgres/postgres.dart';
 
 class PostgresConnection {
-
   static Connection? _conn;
 
-  static Future<Connection> openConnection() async{
+  static Future<Connection> openConnection() async {
     //ip needs to be 10.0.2.2, since if we use localhost or 127.0.0.1, it would be the EMULATOR DEVICE'S ip
-    _conn =  await Connection.open(Endpoint(
-      host: 'autorack.proxy.rlwy.net',
-      port: 36774,
-      database: 'railway',
-      username: 'postgres',
-      password: 'CijHjbfbEobywRyxenLVEoxmplRsxgJd',
-      
-    ));
+    _conn = await Connection.open(
+        Endpoint(
+          host: 'localhost',
+          port: 5432,
+          database: 'app_citas',
+          username: 'app_citas',
+          password: 'app_citas',
+        ),
+        settings: ConnectionSettings(sslMode: SslMode.disable));
     return _conn!;
   }
 
-  static Future<void> closeConnection() async{
+  static Future<void> closeConnection() async {
     await _conn!.close();
   }
 
